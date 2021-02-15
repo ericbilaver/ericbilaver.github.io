61820d056bd583c89abd5ee94d3621668474edcb
Eric Bilaver <ericbilaver@gmail.com>
2021-02-15 18:32:39 -0500

    added basic mobile support

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,21 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    // REMARK plugin needed to extract the markdown files and parses
+
     {
-      resolve: `gatsby-plugin-sharp`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: `src/images/EB.png`,
+        name: "xxx",
+        short_name: "xxxx",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/EB.png", // This path is relative to the root of the site.
       },
     },
   ],
